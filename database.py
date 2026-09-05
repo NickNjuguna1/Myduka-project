18fecfe33ef803d8fb42c403d4a0b5b60433f507
Nickson Njuguna <neennick96@gmail.com>
2026-09-05 20:16:12 +0300

    Update database.py

--- database.py
+++ database.py
@@ -240,7 +240,7 @@ def check_email(email):
 
 
 def total_sales():
-    query = 'select sum(price*quantity) from products inner join sales on sales.product_id=products.product_id;'
+    query = 'select sum(price*quantity) from products inner join sales on sales.product_id=products.id;'
     curr.execute(query)
     data = curr.fetchone()
     return data[0]
