46b7d135d9295f21eb845c1040647fcd419ed75e
Nickson Njuguna <neennick96@gmail.com>
2026-09-07 17:58:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -260,5 +260,5 @@ def edit_product(product_id):
     return render_template('edit_product.html', product=product)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
